d719ecc0ea70d23ce620da5ff36e3415334197c1
SuikaT <bourney.julien@gmail.com>
2025-07-13 00:36:32 +0200

    database install files
    Signed-off-by: SuikaT <bourney.julien@gmail.com>

---
+++ budget-management-backend/src/main/resources/database/tables/create_expense.sql
@@ -0,0 +1,8 @@
+-- budget_management.`user` definition
+CREATE TABLE IF NOT EXISTS `expense` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `amount` float NOT NULL,
+  `category` varchar(25) DEFAULT NULL,
+  `expenseDate` timestamp DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
---
+++ budget-management-backend/src/main/resources/database/tables/create_user.sql
@@ -0,0 +1,9 @@
+-- budget_management.`user` definition
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` char(50) DEFAULT NULL,
+  `lastname` char(50) DEFAULT NULL,
+  `email` char(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
