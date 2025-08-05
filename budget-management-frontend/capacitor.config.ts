8bc137a225b3e63818eaa2c355ed579d9d7d204f
SuikaT <bourney.julien@gmail.com>
2025-08-06 00:52:55 +0200

    Replace react front by angular w/ capacitor

---
+++ budget-management-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.suika.app',
+  appName: 'budget-management-frontend',
+  webDir: 'dist'
+};
+
+export default config;
