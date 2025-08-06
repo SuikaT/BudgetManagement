588045e77a7edff8e9779e4b2858c5df0f7e057d
SuikaT <bourney.julien@gmail.com>
2025-08-06 02:42:08 +0200

    fix capacitor config

---
+++ budget-management-frontend/android/app/src/main/java/com/suika/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.suika.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- budget-management-frontend/capacitor.config.ts
+++ budget-management-frontend/capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.suika.app',
-  appName: 'budget-management-frontend',
-  webDir: 'dist'
+    appId: "com.suika.app",
+    appName: "budget-management-frontend",
+    webDir: "dist/budget-management-frontend/browser",
 };
 
 export default config;
